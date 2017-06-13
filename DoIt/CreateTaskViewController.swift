438a08115623ca31a254394a58358ce900fc4673
Chris Kauth <cskauth@yahoo.com>
2017-06-12 18:04:38 -0700

    First

---
+++ DoIt/CreateTaskViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by Chris Kauth on 6/12/17.
+//  Copyright © 2017 Chris Kauth. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+
+}
---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Chris Kauth on 6/12/17.
+//  Copyright © 2017 Chris Kauth. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
